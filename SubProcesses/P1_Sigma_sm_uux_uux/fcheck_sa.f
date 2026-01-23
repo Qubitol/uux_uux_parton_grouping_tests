f5c9382f4c37d1ccdab0ffe6bf0cde24dce5443d
Daniele Massaro <d.massaro.26@gmail.com>
2026-01-23 16:55:27 +0100

    Line continuation placed in character 5 and not 6

--- SubProcesses/P1_Sigma_sm_uux_uux/fcheck_sa.f
+++ SubProcesses/P1_Sigma_sm_uux_uux/fcheck_sa.f
@@ -66,7 +66,7 @@ C
           GS(IEVT) = 1.2177157847767195 ! fixed G for aS=0.118 (hardcoded for now in check_sa.cc, fcheck_sa.f, runTest.cc)
         END DO
         CALL FBRIDGESEQUENCE_NOMULTICHANNEL(BRIDGE, MOMENTA, GS, ! TEMPORARY? disable multi-channel in fcheck.exe and fgcheck.exe #466
-    &    IFLAV_VEC, RNDHEL, RNDCOL, MES, SELHEL, SELCOL, .FALSE.) ! do not quit after computing helicities
+     &    IFLAV_VEC, RNDHEL, RNDCOL, MES, SELHEL, SELCOL, .FALSE.) ! do not quit after computing helicities
         DO IEVT = 1, NEVT
 c         DO IEXTERNAL = 1, NEXTERNAL
 c           WRITE(6,*) 'MOMENTA', IEVT, IEXTERNAL,
